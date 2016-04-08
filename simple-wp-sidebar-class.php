564e612ea6b5f303e9320f35ff8aad5de1b5e68f
Andy Mayes <andymayes@andmayes.home>
2016-04-08 22:45:34 +0100

    Refactored code and added HTML container parameter

--- simple-wp-sidebar-class.php
+++ simple-wp-sidebar-class.php
@@ -5,15 +5,53 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Simple_Sidebar {
   
-  public $args;  
+  public $args = array();
   
-	function __construct( $args = array() ) {
-  	
-  	$this->args = $args;
-  	
-    add_action( 'widgets_init', array( $this, 'register_sidebar' ) );
+  /**
+	 * Construct
+	 *
+	 * @author Andy Mayes
+	 * @since  1.0.0
+	 *
+	 */
+	function __construct( $args ) {
+  	  $container = $this->use_container( $args );
+  		$this->args = array(
+      	'name'				  => __( isset( $args['name'] ) ? $args['name'] : 'Sidebar' ),
+      	'id'            => isset( $args['id'] ) ? $args['id'] : '',
+      	'description'   => __( isset( $args['description'] ) ? $args['description'] : '' ),
+        'class'         => '',
+      	'before_widget' => '<'.$container.' id="%1$s" class="widget %2$s">',
+      	'after_widget'  => '</'.$container.'>',
+      	'before_title'  => '<h2 class="widget-title">',
+      	'after_title'   => '</h2>',      
+      );
+      
+      add_action( 'widgets_init', array( $this, 'register_sidebar' ) );
   }  
-	
+	/**
+	 * Container
+	 *
+	 * @author Andy Mayes
+	 * @since  1.1.0
+	 *
+	 */
+	function use_container( $args ) {
+  	$this->container = $args['container'];
+  	
+  	if ( $this->container == 'div') {
+    	$this->container = 'div';
+  	}
+    else if ( $this->container == 'aside') {
+    	$this->container = 'aside';
+  	}
+  	else {
+    	$this->container = 'section';
+  	}
+
+  	return $this->container;
+  }
+  	
 	/**
 	 * Register the Sidebar
 	 *
@@ -22,20 +60,6 @@ class Simple_Sidebar {
 	 *
 	 */
 	function register_sidebar() {
-    $args = array_merge(
-      array(
-      	'name'				  => __( isset( $args['name'] ) ? $args['name'] : 'Sidebar' ),
-      	'id'            => isset( $args['id'] ) ? $args['id'] : '',
-      	'description'   => __( isset( $args['description'] ) ? $args['description'] : '' ),
-        'class'         => '',
-      	'before_widget' => '<section id="%1$s" class="widget %2$s">',
-      	'after_widget'  => '</section>',
-      	'before_title'  => '<h2 class="widget-title">',
-      	'after_title'   => '</h2>', 
-      ),
-      $this->args
-    );
-		
-		register_sidebar( $args );
+		register_sidebar( $this->args );
 	}
 }
